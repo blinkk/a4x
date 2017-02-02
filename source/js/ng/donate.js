8414432ba3c2e656f172eeb0bc6afa891b388502
Jeremy Weinstein <jeremydw@gmail.com>
2017-02-02 01:13:22 -0800

    Work on order form.

--- source/js/ng/donate.js
+++ source/js/ng/donate.js
@@ -1,17 +1,23 @@
 var DonateController = function($element, $scope) {
   this.dirty = false;
+  this.customizing = false;
   this.amount = '';
 };
 
 
+DonateController.prototype.toggleCustomizer = function() {
+  this.customizing = !this.customizing;
+};
+
+
 DonateController.prototype.isAmountEqual = function(amount) {
-  return (this.amount == amount) && amount;
+  return !this.dirty || (this.amount == amount);
 };
 
 
 DonateController.prototype.setAmount = function(amount) {
-  this.amount = amount;
   this.dirty = true;
+  this.amount = amount;
 };
 
 
