26405b2323788ce03662ee2d528c89ac106c54b0
Jeremy Weinstein <jeremydw@gmail.com>
2017-02-13 12:13:10 -0800

    Header button hidden on scroll when pay button visible.

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
+var classes = require('airkit/utils/classes');
 var order = require('./ng/order');
 var share = require('./ng/share');
 var smoothScroll = require('smooth-scroll');
+var ui = require('airkit/ui');
 
 
 var a4x = window.a4x || {};
@@ -28,6 +30,21 @@ function initNg() {
       .config(['$interpolateProvider', function($interpolateProvider) {
         $interpolateProvider.startSymbol('[[').endSymbol(']]');
       }])
+      .run(function($rootScope) {
+        // Hide the header donate button when the 'pay' button is visible.
+        var update = function() {
+          var donateHeader = document.getElementById('donate-header');
+          var donateSentinel = document.getElementById('donate-sentinel');
+          if (!donateHeader || !donateSentinel) {
+            return;
+          }
+          var enabled = ui.isElementInView(donateSentinel, undefined, true);
+          classes.enable(donateHeader, 'header__aside__content--hidden', enabled);
+        };
+        window.addEventListener('resize', update);
+        window.addEventListener('scroll', update);
+        window.setTimeout(update, 10);
+      })
       .filter('int', function() {
 	return function(input) {
           return parseInt(input);
